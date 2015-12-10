9bd33d3c40c2f7edc4cb3b7d8628c0e59a3f0da4
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2015-12-09 18:36:28 -0800

    Tweaked awk scripts to work

--- setup/modify_makefiles.sh
+++ setup/modify_makefiles.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-awk '/^TOOLS =/{print;print "\t\therbgrind \\ ";next}1' ../valgrind/Makefile.am > ../valgrind/Makefile.am
-awk '/^AC_CONFIG_FILES([/{print;print "   herbgrind/Makefile";print "   herbgrind/docs/Makefile";print "   herbgrind/tests/Makefile";next}1' ../valgrind/configure.ac > ../valgrind/Makefile.am
+awk -i inplace '/^TOOLS =/{print;print "\t\therbgrind \\";next}1' ../valgrind/Makefile.am
+awk -i inplace '/^AC_CONFIG_FILES/ && !seen {print;print "   herbgrind/Makefile";print "   herbgrind/docs/Makefile";print "   herbgrind/tests/Makefile";seen=1;next}1' ../valgrind/configure.ac
