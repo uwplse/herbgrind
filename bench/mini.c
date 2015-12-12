406b869b90be023d06395d10ddd52b5a19481594
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2015-12-12 15:47:59 -0800

    Add client calls to mini.c
    This way we can analyze the part of the code that's interesting and
    relatively simple

--- bench/mini.c
+++ bench/mini.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <math.h>
+#include "herbgrind.h"
 
 int main(int argc, char** argv){
   double x,y;
+  HERBGRIND_BEGIN();
   x = 3;
   y = sqrt(x + 1);
+  HERBGRIND_END();
   printf("%e\n", y);
   return 0;
 }
