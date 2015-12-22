1d57078b8e621d0d432d17599a41a82008f7fc0b
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2015-12-22 14:54:36 -0800

    Added patch gmp script

---
+++ setup/patch_gmp.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+
+# Comment out uses of malloc, realloc, and free in the default memory
+# functions for gmp. We'll be calling mp_set_memory_functions to
+# override the default memory functions in our tool anyway, so the
+# default ones wil never be called. But the linker will still try and
+# link them, and since valgrind doesn't provide a standard c library
+# to it's tools, it will fail, killing the build. So, we comment them
+# out instead, and everything works fine.
+sed -i \
+    -e 's/malloc.*(.*).*;/0;\/\/&/' \
+    -e 's/realloc.*(.*).*;/0;\/\/&/' \
+    -e 's/free.*(.*).*;/\/\/&/' \
+    -e 's/fprintf.*(.*).*;/\/\/&/'\
+    ../gmp/memory.c
