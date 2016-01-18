ac7003865a7d2c091d86a30ab7f59777ccf87f3b
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-01-17 21:33:20 -0800

    Added evaluateOpError_helper description comment

--- herbgrind/hg_evaluate.h
+++ herbgrind/hg_evaluate.h
@@ -10,6 +10,11 @@ unsigned long long ulpd(double x, double y);
 
 // Evaluate the error of an executed operation.
 void evaluateOpError(ShadowValue* shadowVal, double actualVal);
+// A helper that makes it easier to extract the value from some arg
+// bytes with the type and index. This will reinterpret valbytes as an
+// array of whatever datatype bytestype indicates, and then index into
+// it with el_index to get the actual floating point value that we'll
+// test against the shadow value.
 void evaluateOpError_helper(ShadowValue* shadowVal, UWord* valbytes, LocType bytestype, int el_index)
 
 #endif
