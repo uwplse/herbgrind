dc13bcac2e5d2344862ae6b70c8026bd5cc0aaec
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-01-21 16:09:18 -0800

    Actually added macros.h - oops!

---
+++ herbgrind/macros.h
@@ -0,0 +1,22 @@
+#ifndef _MACROS
+#define _MACROS
+
+#define PRINTBLOCKS
+/* #define PRINTERRORS */
+/* #define PRINTMALLOCS */
+/* #define CHECKMALLOC 0x0000000802d8c455 */
+
+#ifdef PRINTMALLOCS
+#define ALLOC(dest, name, num_elems, elem_size)         \
+  dest = VG_(calloc)(name, num_elems, elem_size);       \
+                    VG_(printf)("Allocing for %s at %p\n", name, dest);
+#elif defined CHECKMALLOC
+#define ALLOC(dest, name, num_elems, elem_size)         \
+  dest = VG_(calloc)(name, num_elems, elem_size);       \
+                    if (dest == CHECKMALLOC) VG_(printf)("Allocing for %s at %p.\n", name, dest);
+#else
+#define ALLOC(dest, name, num_elems, elem_size)  \
+  dest = VG_(calloc)(name, num_elems, elem_size);
+#endif
+
+#endif
