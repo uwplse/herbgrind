0db36bd01a2c8903e090ad454a6750211ff89b6f
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-01-29 18:17:32 -0800

    Added wrapper for pow

--- herbgrind/hg_mathwrap.c
+++ herbgrind/hg_mathwrap.c
@@ -3,11 +3,23 @@
 #include "hg_mathreplace_funcs.h"
 #include "pub_tool_redir.h"
 
-double VG_WRAP_FUNCTION_ZU(libmZdsoZd6, sqrt)(double x);
-double VG_WRAP_FUNCTION_ZU(libmZdsoZd6, sqrt)(double x){
+#define LIBM libmZdsoZd6
+
+double VG_WRAP_FUNCTION_ZU(LIBM, sqrt)(double x);
+double VG_WRAP_FUNCTION_ZU(LIBM, sqrt)(double x){
   double result;
   double args[1];
   args[0] = x;
   HERBGRIND_PERFORM_OP(OP_SQRT, &result, args);
+  return result;
+}
+
+double VG_WRAP_FUNCTION_ZU(LIBM, pow)(double x, double y);
+double VG_WRAP_FUNCTION_ZU(LIBM, pow)(double x, double y){
+  double result;
+  double args[2];
+  args[0] = x;
+  args[1] = y;
+  HERBGRIND_PERFORM_OP(OP_POW, &result, args);
   return result;
 }
