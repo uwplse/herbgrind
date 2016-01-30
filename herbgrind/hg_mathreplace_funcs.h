88ecd55818b87480b9035f8578135dfa622d25df
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-01-29 18:17:24 -0800

    Added the rest of the op enums

--- herbgrind/hg_mathreplace_funcs.h
+++ herbgrind/hg_mathreplace_funcs.h
@@ -3,6 +3,25 @@
 
 typedef enum {
   OP_SQRT,
+  OP_POW,
+  OP_EXP,
+  OP_LOG,
+  OP_COS,
+  OP_SIN,
+  OP_TAN,
+  OP_COTAN,
+  OP_ASIN,
+  OP_ACOS,
+  OP_ATAN,
+  OP_SINH,
+  OP_COSH,
+  OP_TANH,
+  OP_ATAN2,
+  OP_ABS,
+  OP_MOD,
+  OP_EXPM1,
+  OP_LOG1P,
+  OP_HYPOT
 } OpType;
 
 #endif
