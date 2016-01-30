5b9a136e99c294646c91ac9b6a97fd4872458f3d
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-01-30 02:35:07 -0800

    Formalize function list

--- herbgrind/hg_mathreplace_funcs.h
+++ herbgrind/hg_mathreplace_funcs.h
@@ -1,26 +1,31 @@
 #ifndef _HG_MATHREPLACE_FUNCS
 #define _HG_MATHREPLACE_FUNCS
 
+// These operations are based on the ones that Herbie supports natively.
+
 typedef enum {
+  // Unary functions
   OP_SQRT,
-  OP_POW,
   OP_EXP,
   OP_LOG,
   OP_COS,
   OP_SIN,
   OP_TAN,
-  OP_COTAN,
+  /* OP_COTAN, */ // Looks like libm doesn't have this one.
   OP_ASIN,
   OP_ACOS,
   OP_ATAN,
   OP_SINH,
   OP_COSH,
   OP_TANH,
-  OP_ATAN2,
   OP_ABS,
-  OP_MOD,
   OP_EXPM1,
   OP_LOG1P,
+
+  // Binary
+  OP_MOD,
+  OP_POW,
+  OP_ATAN2,
   OP_HYPOT
 } OpType;
 
