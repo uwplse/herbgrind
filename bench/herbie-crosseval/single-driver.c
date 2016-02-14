eb2418178692cb4d29c574b79acd0665e137cb5a
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-02-13 17:52:24 -0800

    Added a driver for running a bench once
    On inputs passed from the command line

---
+++ bench/herbie-crosseval/single-driver.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include "herbgrind.h"
+#include "driver_macros.h"
+
+double f_id(ARGS(double));
+
+int main(int argc, char** argv){
+  double args[NARGS];
+  double result;
+
+  if (argc < NARGS + 1){
+    printf("Hey! You forgot to pass the arguments.\n");
+    exit(1);
+  }
+  // Parse the arguments from the command line.
+  for(int i = 0; i < NARGS; ++i){
+    args[i] = atof(argv[i+1]);
+  }
+
+  HERBGRIND_BEGIN();
+  result = INVOKE(f_id, args);
+  HERBGRIND_END();
+}
