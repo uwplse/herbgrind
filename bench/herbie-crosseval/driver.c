44d32d2825b9b7e9d29fbb15053470131c755b74
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-02-18 15:44:26 -0800

    Overhaul herbie-crosseval

--- bench/herbie-crosseval/driver.c
+++ bench/herbie-crosseval/driver.c
@@ -5,59 +5,28 @@
 #include "herbgrind.h"
 #include "driver-macros.h"
 
-double f_id(ARGS(double));
-double f_im(ARGS(double));
-void setup_mpfr_f_im();
+#ifndef NPTS
+#define NPTS 8000
+#endif
 
-double rand_double() {
-        long long c0 = rand()&0xffff;
-        long long c1 = rand()&0xffff;
-        long long c2 = rand()&0xffff;
-        long long c3 = rand()&0xffff;
-        long long c = ((c3 << 48) | (c2 << 32) | (c1<<16) | c0);
-        return *(double*)&c;
-}
-char ordinaryd(double x) {
-        return 1 / x != 0 && x == x;
-}
+double f_id(ARGS(double));
+
+extern double pts[NPTS][NARGS];
 
 int main(int argc, char** argv){
-  double args[NARGS];
   double result;
-  bool ordinaryArgs;
-  unsigned int num_samples = 8000;
   double min_input, max_input;
-  // Parse a num_samples argument, if it's there.
-  if (argc > 1) {
-    num_samples = atoi(argv[1]);
-  }
-
-  setup_mpfr_f_im();
 
   // For each sample
-  for (int i = 0; i < num_samples; ++i){
-    // Get some random arguments. If they end up including nans or
-    // infs, just keep getting them until they dont. If the result
-    // computed with floats would be nan or inf, do the same.
-    bool valid_args = false;
-    do {
-      RANDARGS(args);
-      valid_args = true;
-      for (int j = 0; j < NARGS; ++j){
-        if (!ordinaryd(args[j])) valid_args = false;
-        else if (!ordinaryd(INVOKE(f_im, args))) valid_args = false;
-      }
-      if (valid_args){
-        for (int j = 0; j < NARGS; ++j){
-          if (args[j] > max_input) max_input = args[j];
-          if (args[j] < min_input) min_input = args[j];
-        }
-      }
-    } while (!valid_args);
+  for (int i = 0; i < NPTS; ++i){
+    for (int j = 0; j < NARGS; j++){
+      if (pts[i][j] > max_input) max_input = pts[i][j];
+      if (pts[i][j] < min_input) min_input = pts[i][j];
+    }
 
     // Run the program on the args
     HERBGRIND_BEGIN();
-    result = INVOKE(f_id, args);
+    result = INVOKE(f_id, pts[i]);
     HERBGRIND_END();
 
     /* printf(" --> %e\n", result); */
