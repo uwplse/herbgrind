40da4e6e1ceda93818384ed3b0e58578f3a9ffd6
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-04-26 11:05:26 -0700

    Don't print the weird negs at the beginning

--- herbgrind/runtime/hg_op_tracker.c
+++ herbgrind/runtime/hg_op_tracker.c
@@ -117,6 +117,7 @@ void writeReport(const HChar* filename){
     Op_Info* opinfo = *(Op_Info**)VG_(indexXA)(tracked_ops, i);
 
     if (opinfo == NULL) continue;
+    if (opinfo->debuginfo.fnname == NULL) continue;
 
     UInt entry_len;
     char* astString = opASTtoString(opinfo->ast);
