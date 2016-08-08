9ad73eddb9f198b5900bb6064c1639960caed0ce
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-08-08 12:45:13 -0700

    Fixed var-test.c so it doesn't get optimized

--- bench/var-test.c
+++ bench/var-test.c
@@ -6,10 +6,12 @@ double __attribute__ ((noinline)) add(double x, double y){
   return x + y;
 }
 int main(int argc, char** argv){
-  volatile double x, y, z;
+  volatile double x, y, z1, z2, z3;
   HERBGRIND_BEGIN();
-  z = 5;
+  z1 = 5;
+  z2 = 6;
+  z3 = z1 + z2;
   x = add(4, 5);
-  y = add(6, z + 6);
+  y = add(6, z3);
   HERBGRIND_END();
 }
