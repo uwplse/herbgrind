433b6d5d0ad0232353fa2474fb647454225cb6ee
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-08-23 17:12:23 -0700

    Work when gawk doesn't support '-i inplace'

--- setup/modify_makefiles.sh
+++ setup/modify_makefiles.sh
@@ -31,10 +31,14 @@
 #
 
 # Modify the valgrind makefiles to include the herbgrind directory
-gawk -i inplace '/^TOOLS =/{print;print "\t\therbgrind \\";next}1' ../valgrind/Makefile.am
-gawk -i inplace '/^AC_CONFIG_FILES/ && !seen {print;print "   herbgrind/Makefile";print "   herbgrind/docs/Makefile";print "   herbgrind/tests/Makefile";seen=1;next}1' ../valgrind/configure.ac
-gawk -i inplace '/^TOOL_DEPENDENCIES_@VGCONF_PLATFORM_PRI_CAPS@ =/{print;print "\t$(extra_deps_amd64) \\";next}1' ../valgrind/Makefile.tool.am
-gawk -i inplace '/^TOOL_DEPENDENCIES_@VGCONF_PLATFORM_SEC_CAPS@ =/{print;print "\t$(extra_deps_i386) \\";next}1' ../valgrind/Makefile.tool.am
+gawk '/^TOOLS =/{print;print "\t\therbgrind \\";next}1' ../valgrind/Makefile.am > ../valgrind/Makefile.am.tmp
+mv ../valgrind/Makefile.am.tmp ../valgrind/Makefile.am
+gawk '/^AC_CONFIG_FILES/ && !seen {print;print "   herbgrind/Makefile";print "   herbgrind/docs/Makefile";print "   herbgrind/tests/Makefile";seen=1;next}1' ../valgrind/configure.ac > ../valgrind/configure.ac.tmp
+mv ../valgrind/configure.ac.tmp ../valgrind/configure.ac
+gawk '/^TOOL_DEPENDENCIES_@VGCONF_PLATFORM_PRI_CAPS@ =/{print;print "\t$(extra_deps_amd64) \\";next}1' ../valgrind/Makefile.tool.am > ../valgrind/Makefile.tool.am.tmp
+mv ../valgrind/Makefile.tool.am.tmp ../valgrind/Makefile.tool.am.tmp
+gawk '/^TOOL_DEPENDENCIES_@VGCONF_PLATFORM_SEC_CAPS@ =/{print;print "\t$(extra_deps_i386) \\";next}1' ../valgrind/Makefile.tool.am > ../valgrind/Makefile.tool.am.tmp
+mv ../valgrind/Makefile.tool.am.tmp ../valgrind/Makefile.tool.am
 
 # Comment out uses of fprintf in assert.c in gmp, since running on top
 # of the valgrind c library it doesn't work. Not sure if we actually
