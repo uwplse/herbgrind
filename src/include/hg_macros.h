feb3f24bb963182201eff429c6825a8a8101bb22
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-10-09 00:01:45 -0700

    Fail on null malloc

--- src/include/hg_macros.h
+++ src/include/hg_macros.h
@@ -45,7 +45,8 @@ extern uintptr_t maxptr;
   dest = VG_(calloc)(name, num_elems, elem_size);       \
                     if (print_mallocs) VG_(printf)("Allocing for %s at %p\n", name, dest); \
                     if ((intptr_t)dest < minptr) minptr = (intptr_t)dest; \
-                    if ((intptr_t)dest > maxptr) maxptr = (intptr_t)dest;
+                    if ((intptr_t)dest > maxptr) maxptr = (intptr_t)dest; \
+                    tl_assert(dest != NULL);
 #define DEBUG(...) VG_(printf)(__VA_ARGS__)
 #define CHECK_PTR(ptr) tl_assert2(ptr == NULL || (minptr <= (intptr_t)ptr && (intptr_t)ptr <= maxptr), "Bad pointer %p (range is [%p to %p])", ptr,minptr, maxptr);
 #endif
