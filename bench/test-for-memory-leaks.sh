b8a61624c0e64b97bd7ecd2b263b34e016fece6f
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-10-14 13:56:19 -0700

    Change script to also look for leaked stems

--- bench/test-for-memory-leaks.sh
+++ bench/test-for-memory-leaks.sh
@@ -3,11 +3,18 @@
 for f in *.out; do
     echo "testing file $f"
     ../valgrind/herbgrind-install/bin/valgrind --tool=herbgrind --print-moves ./$f &>output.log
-    NUM_ALLOCED=$(grep -c "Making shadow value" $f)
-    NUM_FREED=$(grep -c "Cleaning up shadow value" $f)
-    if [ "$NUM_ALLOCED" -ne "$NUM_FREED" ]
+    NUM_SV_ALLOCED=$(grep -c "Making shadow value" output.log)
+    NUM_SV_FREED=$(grep -c "Cleaning up shadow value" output.log)
+    if [ "$NUM_SV_ALLOCED" -ne "$NUM_SV_FREED" ]
     then
-        echo "Found memory leak when testing against $f! see output.log for output."
-        break
+        echo "Found shadow value leak when testing against $f! see output.log for output."
+        exit 1
+    fi
+    NUM_STEM_ALLOCED=$(grep -c "Making stem node" output.log)
+    NUM_STEM_FREED=$(grep -c "Cleaning up stem node" output.log)
+    if [ "$NUM_STEM_ALLOCED" -ne "$NUM_STEM_FREED" ]
+    then
+        echo "Found stem leak when testing against $f! see output.log for output."
+        exit 1
     fi
 done
