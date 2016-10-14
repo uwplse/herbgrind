52f857b0355c57f46058939d159e6efe975fcbb6
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-10-13 19:03:04 -0700

    Fixed band-aid warning
    It was warning whenever there WASN'T a problem. Oops...

--- src/runtime/hg_storage_runtime.c
+++ src/runtime/hg_storage_runtime.c
@@ -383,7 +383,7 @@ void setLoc__(Addr index, ShadowLocation* newLoc, LocType move_type,
   /*            "but the move had a type with capacity %lu, " */
   /*            "and the location only has capacity %lu\n", */
   /*            newLoc, capacity(move_type), capacity(newLoc->type)); */
-  if (newLoc != NULL && capacity(move_type) <= capacity(newLoc->type))
+  if (newLoc != NULL && capacity(move_type) > capacity(newLoc->type))
     VG_(printf)("Bad location type found (when moving from temp to memory/thread state)!!\n");
   if (newLoc == NULL || move_type != newLoc->type){
     for (SizeT i = 0; i < capacity(move_type); ++i){
