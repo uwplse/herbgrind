4a0822b006a415e65ebf27c4df8d01842e166672
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-10-19 10:18:20 -0700

    Use run-herbgrind for test-gromacs.sh

--- test/test-herbgrind-gromacs.sh
+++ test/test-herbgrind-gromacs.sh
@@ -7,7 +7,7 @@ then
     exit $?
 fi
 cd $SPEC_DIR/benchspec/CPU2006/435.gromacs/run/run_base_test_amd64-m64-gcc43-nn.0003
-if (timeout -m 17000000 --confess $HERBGRIND_DIR/valgrind/herbgrind-install/bin/valgrind --tool=herbgrind $* ./gromacs_base.amd64-m64-gcc43-nn -silent -deffnm gromacs -nice 0)
+if (timeout -m 17000000 --confess $HERBGRIND_DIR/test/run-herbgrind.sh $* ./gromacs_base.amd64-m64-gcc43-nn -silent -deffnm gromacs -nice 0)
 then
     echo "Success"
 else
