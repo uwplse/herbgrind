f4fd962975f1ce0821f0c3d7c1bb54b2c88f20b3
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2016-10-21 12:19:12 -0700

    Add more location coercion cases

--- src/runtime/hg_shadowop.c
+++ src/runtime/hg_shadowop.c
@@ -1279,9 +1279,12 @@ ShadowLocation* getShadowLocation(UWord tmp_num, LocType type){
         VG_(printf)("Got non-null shadow location at %lu\n", tmp_num);
       return location;
     } else if ((location->type == Lt_Double &&
-           type == Lt_Doublex2) ||
-          (location->type == Lt_Float &&
-           type == Lt_Floatx4)){
+                type == Lt_Doublex2) ||
+               ((location->type == Lt_Float ||
+                 location->type == Lt_Floatx2) &&
+                type == Lt_Floatx4) ||
+               (location->type == Lt_Float &&
+                type == Lt_Floatx2){
       ShadowLocation* newLocation = mkShadowLocation_bare(type);
       newLocation->values[0] = location->values[0];
       setTemp(tmp_num, newLocation);
