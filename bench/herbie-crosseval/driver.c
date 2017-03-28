f98c6b529c9a194c7336afc0be506e3e1244c5fc
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2017-03-28 16:18:50 +0200

    Mark outputs in herbie-crosseval

--- bench/herbie-crosseval/driver.c
+++ bench/herbie-crosseval/driver.c
@@ -15,19 +15,14 @@ extern double pts[NPTS][NARGS];
 
 int main(int argc, char** argv){
   double result;
-  double min_input, max_input;
 
   // For each sample
   for (int i = 0; i < NPTS; ++i){
-    for (int j = 0; j < NARGS; j++){
-      if (pts[i][j] > max_input) max_input = pts[i][j];
-      if (pts[i][j] < min_input) min_input = pts[i][j];
-    }
-
     // Run the program on the args
     HERBGRIND_BEGIN();
     result = INVOKE(f, pts[i]);
     HERBGRIND_END();
+    HERBGRIND_MARK_IMPORTANT(result);
 
     /* printf(" --> %e\n", result); */
   }
