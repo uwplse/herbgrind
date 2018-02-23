7c058a102954975670eb74f5f78557538c559c35
Pavel Panchekha <me@pavpanchekha.com>
2018-02-22 19:31:31 -0800

    Missed a prototype, I guess

--- src/runtime/value-shadowstate/value-shadowstate.h
+++ src/runtime/value-shadowstate/value-shadowstate.h
@@ -149,6 +149,14 @@ ShadowValue* getTS(Int idx);
 VG_REGPARM(2) void printStoreValue(const char* dest_label, ShadowValue* val);
 void printStoreValueF(ShadowValue* val, const char* format, ...);
 
+ShadowValue* mkShadowValueBare_fast(FloatType type);
+ShadowValue* mkShadowValue_fast(FloatType type, double value);
+void freeShadowValue_fast(ShadowValue* val);
+void freeShadowTemp_fast(ShadowTemp* temp);
+void disownNonNullShadowValue(ShadowValue* val);
+void ownNonNullShadowValue(ShadowValue* val);
+void disownShadowTemp_fast(ShadowTemp* temp);
+
 __attribute__((always_inline))
 inline
 ShadowValue* mkShadowValueBare_fast(FloatType type){
@@ -162,14 +170,6 @@ ShadowValue* mkShadowValueBare_fast(FloatType type){
   result->ref_count = 1;
   return result;
 }
-
-ShadowValue* mkShadowValue_fast(FloatType type, double value);
-void freeShadowValue_fast(ShadowValue* val);
-void freeShadowTemp_fast(ShadowTemp* temp);
-void disownNonNullShadowValue(ShadowValue* val);
-void ownNonNullShadowValue(ShadowValue* val);
-void disownShadowTemp_fast(ShadowTemp* temp);
-
 __attribute__((always_inline))
 inline
 ShadowValue* mkShadowValue_fast(FloatType type, double value){
@@ -177,7 +177,6 @@ ShadowValue* mkShadowValue_fast(FloatType type, double value){
   setReal_fast(result->real, value);
   return result;
 }
-
 __attribute__((always_inline))
 inline
 void freeShadowValue_fast(ShadowValue* val){
