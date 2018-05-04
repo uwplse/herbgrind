b35890a0ae581e7b8da7745be6eb122e9b74fed9
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2018-05-04 12:54:46 -0700

    More verbose test runs

--- bench/test.py
+++ bench/test.py
@@ -10,7 +10,7 @@ def compare_results(actual, expected):
 
 def test(prog):
     global success
-    command = ["./valgrind/herbgrind-install/bin/valgrind", "--tool=herbgrind", "--output-sexp", prog]
+    command = ["./valgrind/herbgrind-install/bin/valgrind", "--tool=herbgrind", "--output-sexp", "--print-inputs", "--print-errors", prog]
     print("Calling `{}`...".format(" ".join(command)), end="")
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
