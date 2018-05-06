30c181f697ee66cb5abc805759d3657859f648ac
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2018-05-06 14:54:50 -0700

    Some benchmark tweaks

--- bench/fma.c
+++ bench/fma.c
@@ -2,11 +2,10 @@
 #include <math.h>
 
 int main() {
-  volatile double x1, y1, z1;
-  double x, y, z, w;
+  volatile double x1, y1, z1, w;
   x1 = 1 + 1e-15;
   y1 = 1 + 1e-15;
   z1 = 1;
-  w = fma(x, y, z);
+  w = fma(x1, y1, z1);
   printf("%g\n", w);
 }
